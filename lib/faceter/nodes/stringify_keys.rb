d0c1f8d36161ed2000d8f14533bb968a3da80f1c
Andrew Kozin <andrew.kozin@gmail.com>
2015-07-09 15:25:25 +0300

    [INTERNAL] Refactor keys transformation nodes
    Add `TransformKeys` node as the base class for stringifying and symbolizing keys.
    
    == Added
    
    * Faceter::Nodes::TransformKeys
    
    == Updated (backward-compatible)
    
    * Faceter::Nodes::StringifyKeys
    * Faceter::Nodes::SymbolizeKeys

--- lib/faceter/nodes/stringify_keys.rb
+++ lib/faceter/nodes/stringify_keys.rb
@@ -6,7 +6,7 @@ module Faceter
     #
     # @api private
     #
-    class StringifyKeys < AbstractMapper::Node
+    class StringifyKeys < TransformKeys
 
       # @!scope class
       # @!method new(options)
@@ -23,30 +23,7 @@ module Faceter
       #
       # @return [Faceter::Nodes::StringifyKeys]
 
-      # @private
-      def initialize(**options)
-        @options = options
-        @nested  = options.fetch(:nested) { true }
-        super
-      end
-
-      # Transformer function, defined by the node
-      #
-      # @return [Transproc::Function]
-      #
-      def transproc
-        Functions[:transform_hash, @nested, __transformation__]
-      end
-
       private
-
-      def __transformation__
-        Functions[:map_keys, __filtered__]
-      end
-
-      def __filtered__
-        Functions[:guard, Functions[:check, @options], __function__]
-      end
 
       def __function__
         -> key { key.to_s }
--- lib/faceter/nodes/symbolize_keys.rb
+++ lib/faceter/nodes/symbolize_keys.rb
@@ -6,7 +6,7 @@ module Faceter
     #
     # @api private
     #
-    class SymbolizeKeys < AbstractMapper::Node
+    class SymbolizeKeys < TransformKeys
 
       # @!scope class
       # @!method new(options)
@@ -23,30 +23,7 @@ module Faceter
       #
       # @return [Faceter::Nodes::SymbolizeKeys]
 
-      # @private
-      def initialize(**options)
-        @options = options
-        @nested  = options.fetch(:nested) { true }
-        super
-      end
-
-      # Transformer function, defined by the node
-      #
-      # @return [Transproc::Function]
-      #
-      def transproc
-        Functions[:transform_hash, @nested, __transformation__]
-      end
-
       private
-
-      def __transformation__
-        Functions[:map_keys, __filtered__]
-      end
-
-      def __filtered__
-        Functions[:guard, Functions[:check, @options], __function__]
-      end
 
       def __function__
         -> key { key.to_s.to_sym }
